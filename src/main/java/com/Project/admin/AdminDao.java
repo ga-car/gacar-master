8d91c0f9c71664cbd9e8847dcb83159830e48a30
kjy0922 <kjy0922@gmail.com>
2016-10-26 21:38:48 +0900

    Revert "커밋-양희성"
    This reverts commit 9bcf78e3061246e0d99bd7780396883b682cb77e.

--- src/main/java/com/Project/admin/AdminDao.java
+++ src/main/java/com/Project/admin/AdminDao.java
@@ -1,13 +1,9 @@
 package com.Project.admin;
 
-import java.util.List;
-
 import com.Project.rentacar.RentacarModel;
 
 public interface AdminDao {
 
-	int insertRentacar(RentacarModel reatacarModel);
-
-	List<RentacarModel> RentacarList();
+	public int insertRentacar(RentacarModel reatacarModel);
 
 }
--- src/main/java/com/Project/admin/AdminService.java
+++ src/main/java/com/Project/admin/AdminService.java
@@ -1,26 +1,16 @@
 package com.Project.admin;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.mybatis.spring.SqlSessionTemplate;
 
-import com.Project.notify.NotifyModel;
 import com.Project.rentacar.RentacarModel;
 
 public class AdminService implements AdminDao {
 
-	@Resource
+	@Resource(name = "sqlSessionTemplate")
 	private SqlSessionTemplate sqlSessionTemplate;
-	
-	//글목록
-	@Override
-	public List<RentacarModel> RentacarList() {
-		return sqlSessionTemplate.selectList("admin.rentacarList"); 
-	}
 
-	//글쓰기
 	@Override
 	public int insertRentacar(RentacarModel reatacarModel) {
 		return sqlSessionTemplate.insert("admin.insertRentacar", reatacarModel);
