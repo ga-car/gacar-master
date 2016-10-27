0f890bca63762e5d940e2b6f37894fddd3973cf2
kimjaeyoung <kjy0922@gmail.com>
2016-10-27 10:54:23 +0900

    Revert "혜경_notify model 수정"
    This reverts commit d955996c2f489c2c062572de0bf00b6f4cbdcec5.

--- src/main/java/com/Project/notify/NotifyModel.java
+++ src/main/java/com/Project/notify/NotifyModel.java
@@ -1,5 +1,9 @@
 package com.Project.notify;
 
+<<<<<<< HEAD
+public class NotifyModel {
+
+=======
 import java.util.Date;
 
 public class NotifyModel {
@@ -46,6 +50,6 @@ public class NotifyModel {
 	}
 	public void setReadcount(int readcount) {
 		this.readcount = readcount;
-	}/*
->>>>>>> refs/remotes/origin/master*/
+	}
+>>>>>>> refs/remotes/origin/master
 }
