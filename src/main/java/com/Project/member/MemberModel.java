e92ddec870437713bc4a092d16c19af415d855d3
Miryung-Lee <rrryung83@gmail.com>
2016-11-08 19:12:09 +0900

    Miryung - Member(user/mypage/admin) Complete
    Member(user/mypage/admin) Complete

--- src/main/java/com/Project/member/MemberModel.java
+++ src/main/java/com/Project/member/MemberModel.java
@@ -2,8 +2,9 @@ package com.Project.member;
 
 import java.util.Date;
 
-public class MemberModel {
 
+public class MemberModel {
+	
 	private int num;
 	private String email;
 	private String password;
@@ -15,205 +16,162 @@ public class MemberModel {
 	private String addr2;
 	private String jumin1;
 	private String jumin2;
-
+	
 	private String cartype;
 	private String carnum;
-
+	
 	private String licensetype;
 	private String licensenum;
 	private String licensedate;
-
+	
 	private String memlevel;
 	private int point;
 	private int usenum;
 	private Date joindate;
 	private Date accessdate;
-
+	
 	private String del;
 	private Date deldate;
-
+	
 	public int getNum() {
 		return num;
 	}
-
 	public void setNum(int num) {
 		this.num = num;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getPassword2() {
 		return password2;
 	}
-
 	public void setPassword2(String password2) {
 		this.password2 = password2;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getPhone() {
 		return phone;
 	}
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 	public String getZipcode() {
 		return zipcode;
 	}
-
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
 	}
-
 	public String getAddr() {
 		return addr;
 	}
-
 	public void setAddr(String addr) {
 		this.addr = addr;
 	}
-
 	public String getAddr2() {
 		return addr2;
 	}
-
 	public void setAddr2(String addr2) {
 		this.addr2 = addr2;
 	}
-
 	public String getCartype() {
 		return cartype;
 	}
-
 	public void setCartype(String cartype) {
 		this.cartype = cartype;
 	}
-
 	public String getCarnum() {
 		return carnum;
 	}
-
 	public void setCarnum(String carnum) {
 		this.carnum = carnum;
 	}
-
 	public String getLicensetype() {
 		return licensetype;
 	}
-
 	public void setLicensetype(String licensetype) {
 		this.licensetype = licensetype;
 	}
-
 	public String getLicensenum() {
 		return licensenum;
 	}
-
 	public void setLicensenum(String licensenum) {
 		this.licensenum = licensenum;
 	}
-
 	public String getLicensedate() {
 		return licensedate;
 	}
-
 	public void setLicensedate(String licensedate) {
 		this.licensedate = licensedate;
 	}
-
 	public String getMemlevel() {
 		return memlevel;
 	}
-
 	public void setMemlevel(String memlevel) {
 		this.memlevel = memlevel;
 	}
-
 	public int getPoint() {
 		return point;
 	}
-
 	public void setPoint(int point) {
 		this.point = point;
 	}
-
 	public int getUsenum() {
 		return usenum;
 	}
-
 	public void setUsenum(int usenum) {
 		this.usenum = usenum;
 	}
-
 	public Date getJoindate() {
 		return joindate;
 	}
-
 	public void setJoindate(Date joindate) {
 		this.joindate = joindate;
 	}
-
 	public Date getAccessdate() {
 		return accessdate;
 	}
-
 	public void setAccessdate(Date accessdate) {
 		this.accessdate = accessdate;
 	}
-
 	public String getDel() {
 		return del;
 	}
-
 	public void setDel(String del) {
 		this.del = del;
 	}
-
 	public Date getDeldate() {
 		return deldate;
 	}
-
 	public void setDeldate(Date deldate) {
 		this.deldate = deldate;
 	}
-
 	public String getJumin1() {
 		return jumin1;
 	}
-
 	public void setJumin1(String jumin1) {
 		this.jumin1 = jumin1;
 	}
-
 	public String getJumin2() {
 		return jumin2;
 	}
-
 	public void setJumin2(String jumin2) {
 		this.jumin2 = jumin2;
 	}
+	
+	
 
 }
--- src/main/java/com/Project/mypage/MypageController.java
+++ src/main/java/com/Project/mypage/MypageController.java
@@ -1,5 +1,7 @@
 package com.Project.mypage;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -21,95 +23,101 @@ public class MypageController {
 	private MemberService memberService;
 
 	ModelAndView mav = new ModelAndView();
-
+	
 	@RequestMapping(value = "/mypage.do")
 	public ModelAndView mypageMain(HttpServletRequest request, HttpSession session) {
-
+		
 		if (session.getAttribute("session_email") != null) {
-			mav.setViewName("mypage");
-			return mav;
-		} else {
-			mav.setViewName("loginConfirm");
-			return mav;
-		}
+  			mav.setViewName("mypage");
+  			return mav;
+  		} else {
+  			mav.setViewName("loginConfirm");
+  			return mav;
+  		}
 	}
-
+	
 	@RequestMapping(value = "/memberConfirm.do")
 	public ModelAndView memberConfirm(HttpSession session) {
-
-		String email = (String) session.getAttribute("session_email");
-		MemberModel mem = memberService.getMember(email);
-		mav.addObject("mem", mem);
-		mav.setViewName("memberConfirm");
-		return mav;
+		
+			String email = (String)session.getAttribute("session_email");
+			MemberModel mem = memberService.getMember(email);
+			mav.addObject("mem", mem);
+  			mav.setViewName("memberConfirm");
+  			return mav;
 	}
 
+	
 	@RequestMapping(value = "/memberModify.do")
-	public ModelAndView memberModify(@ModelAttribute("mem") MemberModel mem, BindingResult result, HttpSession session,
-			HttpServletRequest request) {
-		String pwTemp = request.getParameter("password");
-		String email = (String) session.getAttribute("session_email");
+	public ModelAndView memberModify(@ModelAttribute("mem") MemberModel mem, BindingResult result,
+  			HttpSession session, HttpServletRequest request) {
+		String pwTemp = request.getParameter("password");	
+		String email = (String)session.getAttribute("session_email");
 		mem = memberService.getMember(email);
 		String pw = mem.getPassword();
-		mav.addObject("mem", mem);
-		mav.setViewName("memberModify");
-		return mav;
+			mav.addObject("mem", mem);
+			mav.setViewName("memberModify");			
+			return mav;
 	}
-
-	@RequestMapping(value = "/memberModify.do", method = RequestMethod.POST)
+	
+	@RequestMapping(value = "/memberModify.do", method=RequestMethod.POST)
 	public ModelAndView memberModify(@ModelAttribute("mem") MemberModel mem, BindingResult result,
-			HttpSession session) {
-		String email = (String) session.getAttribute("session_email");
+  			HttpSession session) {
+		String email = (String)session.getAttribute("session_email");
 		MemberModel memPre = memberService.getMember(email);
 		String pwPre = memPre.getPassword();
 		mem.setEmail(email);
 		memberService.updateMember(mem);
 		String pw = mem.getPassword();
 
-		if (pw.equals(pwPre)) {
-			mav.setViewName("redirect:memberConfirm.do");
-		} else {
-			session.invalidate();
+		if ( pw.equals(pwPre)) {			
+  			mav.setViewName("redirect:memberConfirm.do");
+  		} else {
+  			session.invalidate();
 			mav.setViewName("memberModifyEnd");
-		}
+  		}
 		return mav;
 	}
-
+	
 	@RequestMapping(value = "/memberDelPre.do")
 	public ModelAndView memberDelPre() {
-		System.out.println("delpre");
+		System.out.println("delpre");		
 		mav.addObject("pwCheck", 0);
 		mav.setViewName("memberDelPre");
 		return mav;
 	}
-
-
-	@RequestMapping(value = "/memberDel.do", method = RequestMethod.POST)
+	
+	@RequestMapping(value = "/memberDel.do", method=RequestMethod.POST)
 	public ModelAndView memberDel(HttpSession session, HttpServletRequest request) {
-
+		
 		System.out.println("delPost");
-		MemberModel mem; 
-
-		String email;
-		String pw;
-		pw = request.getParameter("password");
-		int pwCheck;
-
-		email = session.getAttribute("session_email").toString();
-		mem = (MemberModel) memberService.getMember(email);
-
-		if (mem.getPassword().equals(pw)) {
-			pwCheck = 1;
-			mem.setDel("Y");
-			memberService.updateDelMem(mem);
-			session.invalidate();
-
-		} else {
-			pwCheck = -1; // �н����尡 �ȸ�����
-
-		}
-		mav.addObject("pwCheck", pwCheck);
-		mav.setViewName("memberDelPre");
+		MemberModel mem; // 쿼리 결과 값을 저장할 객체
+ 		
+ 		String email;
+ 		String pw;
+ 		pw = request.getParameter("password");
+ 		int pwCheck;
+ 		
+ 		email = session.getAttribute("session_email").toString();
+ 		mem = (MemberModel) memberService.getMember(email);
+ 		
+ 		
+ 		
+ 		if(mem.getPassword().equals(pw)) {
+ 			//패스워드가 맞으면
+ 			pwCheck = 1;
+ 			Date deldate = new Date();
+ 			//삭제 쿼리 수행 			
+ 			memberService.updateDelMem(email); 			
+ 			/*세션 한 번에 제거*/
+ 			session.invalidate();
+ 			
+ 		}
+ 		else {
+ 			pwCheck = -1; //패스워드가 안맞을때
+ 			
+ 		}
+ 		mav.addObject("pwCheck", pwCheck);
+	 	mav.setViewName("memberDelPre");
 		return mav;
 	}
 
