59e9ddf95f6d29d3f45cdacf9af4b42d30de0860
kyg <gay0236@gmail.com>
2016-11-11 21:07:55 +0900

    카풀 수정 입니다 _ 영균

---
+++ src/main/java/com/Project/carpool/AttendModel.java
@@ -0,0 +1,48 @@
+package com.Project.carpool;
+
+public class AttendModel {
+	
+	private String email;
+	private String name;
+	private String subject;
+	private String content;
+	private int no;
+	private int count;
+	
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
